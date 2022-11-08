b2a47ff205062cd9d3a8d522ef1261e2d91fb8d5
Szade <wojciech.szade@gmail.com>
2022-11-08 21:22:32 +0100

    Small changes in remove_expired_urls function

--- url_shortener/url/management/commands/remove_expired_urls.py
+++ url_shortener/url/management/commands/remove_expired_urls.py
@@ -4,13 +4,18 @@ class Command(BaseCommand):
     help = "Set urls that are older than 30 days to expired"
 
     def handle(self, *args, **options):
-        from url.models import Url
-        from django.utils import timezone
-        import datetime
-        urls = Url.objects.filter(last_access__lte=timezone.now() - datetime.timedelta(days=30))
-        deleted_count = 0
-        for url in urls:
-            url.delete()
-            print("Deleted url: " + url.original_url + " last access: " + str(url.last_access) + " with shorten url: " + url.shorten_url + " pub date: " + str(url.pub_date) + " count: " + str(url.count))
-            deleted_count += 1
-        print("Deleted " + str(deleted_count) + " expired urls")
+        try:
+            from url.models import Url
+            from django.utils import timezone
+            import datetime
+            days = 30 #days till expiration
+            urls = Url.objects.filter(last_access__lte=timezone.now() - datetime.timedelta(days=days))
+            deleted_count = 0
+            for url in urls:
+                url.delete()
+                print("Deleted url: " + url.original_url + " last access: " + str(url.last_access) + " with shorten url: " + url.shorten_url + " pub date: " + str(url.pub_date) + " count: " + str(url.count))
+                deleted_count += 1
+            print("Deleted " + str(deleted_count) + " expired urls")
+        except Exception as e:
+            print("Error: " + str(e))
+        
