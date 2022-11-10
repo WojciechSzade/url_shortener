accd0047efd6c500b9dfabfde6ac858144114576
Szade <wojciech.szade@gmail.com>
2022-11-10 01:52:12 +0100

    Changes html files, adds css
    Complete overkill pretty css formatting

--- url_shortener/url/urls.py
+++ url_shortener/url/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('shorten/<slug:slug>/', views.shorten, name='shorten'),
     path('<slug:slug>/', views.redirect_outside, name='redirect_outside'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
